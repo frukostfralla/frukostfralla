2bbea30d2c62b11fff2fc4d9f929faacc121f515
Karl Mårtensson <kmartensson@gmail.com>
2012-04-05 22:44:20 +0200

    Simple db connection

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 
 var express = require('express');
 
+/* DB CONFIG */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var db = mongoose.connect('mongodb://dev:dev@ds031567.mongolab.com:31567/frukostfralla_karl_development', function(err){
+	console.log("Connection callback: "+ err)
+});
+
+
+console.log('connection done')
+
+var UserSchema = new Schema({
+    firstname  :  { type: String, default: 'hahaha' }
+    ,lastname  :  { type: String, default: 'hahaha' }
+    ,age  :  { type: String, default: 'hahaha' }
+    ,marriage_status:  { type: String, default: 'hahaha' }
+    ,details:  { type: String, default: 'hahaha' }
+    ,remark  :  { type: String, default: 'hahaha' }
+});
+
+
+mongoose.model('User', UserSchema );
+var User = db.model('User', UserSchema);
+
+
+
 /* APP CONFIG */
 
 var app = express.createServer();
@@ -33,6 +60,37 @@ app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   	res.render('home');
 });
+
+/* -- Members -- */
+
+app.get('/members', function (req, res) {
+   var responseString = ""
+    User.find({}, function (err, docs) {
+        docs.forEach(function (doc){
+		    responseString += " " + doc.firstname + " "+ doc.lastname+"<br/>"
+        });
+        res.send(responseString);
+	});
+    
+});
+
+app.get('/members/add', function (req, res) {
+    var record = new User();
+    record.firstname = 'Nisse';
+		record.lastname = req.query.lastname
+    record.age = '44';
+    record.marriage_status = 'gift';
+    record.details = 'Inte mycket';
+    record.remarks = 'Fyller snart 45';
+    record.save(function(err, record) {
+        if (err) {
+    		throw err;
+    	}
+        res.send("Sparad");
+    }); 
+});
+
+
 
 
 /* START */
